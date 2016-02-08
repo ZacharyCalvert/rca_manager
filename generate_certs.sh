2ee6a297ad35b345c2ff7fa4e5d0adbb9acbee1b
Zach Calvert <ZachCalvert@gmail.com>
2016-02-07 21:41:21 -0600

    Began input validation and cleanup

--- generate_certs.sh
+++ generate_certs.sh
@@ -6,14 +6,15 @@
 function usage {
    echo "This script is used to create sample SSL certificates, including support"
    echo "for intermediate certificate chains.  Do not treat this script as a means"
-   echo "to generate your production SSL certificates."
-   echo "Usage: ./<script.sh> [clean|single|intermediate] [Domain]"
+   echo "to generate or manage your production SSL certificates."
+   echo "Usage: ./<script.sh> [clean|single|intermediate] [site.company.TLD]"
    echo "   clean:        cleans (deletes) the ./.ssl directory"
    echo "   single:       Generates a root certificate and a single site SSL cert"
    echo "   intermediate: Generates a root certificate, an intermediate certificate, "
    echo "                 and a site SSL cert."
-   echo "   Domain is your site comain such as mycompany.com.  The first portion"
-   echo "   of the domain (anything before the first .) is treated as your company"
+   echo "   site:         target deploy such as devlocal.mycompany.com.  The "
+   echo "                 values are split up to define the company name and "
+   echo "                 the domain <company.tld> for the admin email address."
    exit 1
 }
 
@@ -58,7 +59,7 @@ default_md      = sha1
 prompt                  = no
 distinguished_name      = root_ca_distinguished_name
 [ root_ca_distinguished_name ]
-commonName              = single-root-ssl.$DOMAIN
+commonName              = root-ssl.$DOMAIN
 stateOrProvinceName     = TX
 countryName             = US
 emailAddress            = IT_SSL@$DOMAIN
@@ -69,6 +70,7 @@ extendedKeyUsage        = serverAuth,clientAuth,codeSigning,msCodeInd,msCodeCom
 basicConstraints        = CA:true
 EOF
 }
+
 function generateSingleCertConf {
 cat <<EOF
 [ req ]
@@ -90,6 +92,43 @@ authorityKeyIdentifier  = keyid,issuer
 basicConstraints        = CA:true
 EOF
 }
-mkdir ./.ssl
+
+function testOpenSsl {
+    command -v openssl >/dev/null 2>&1 || { echo "Openssl is not on the path.  Please install openssl.  Aborting." >&2; exit 1; }
+}
+
+function validateInput {
+    if [ "" == "$1" ] || [ "" == "$2" ] ; then
+        usage
+    fi
+    if [ "single" != "$1" ] && [ "intermediate" != "$1" ] && [ "clean" != "$1" ] ; then
+        usage
+    fi
+    export SERVER=$2
+    export COMPANY=`echo "$2" | awk -F '.' '{print $(NF-1)}'`
+    export TLD=`echo "$2" | awk -F '.' '{print $(NF)}'`
+    export DOMAIN=${COMPANY}.${TLD}
+    export SUPPORT_EMAIL="ssl-admin@$DOMAIN"
+
+    if [ "" == "$COMPANY" ]; then
+        usage
+    fi
+    echo "Server target: $SERVER"
+    echo "Company: $COMPANY"
+    echo "Support contact email: $SUPPORT_EMAIL"
+}
+
+function createSslFolderIfNotExist {
+  if [ ! -d "./.ssl" ]; then
+    mkdir ./.ssl
+    exitOnFailure "Could not create local .ssl directory"
+  fi
+}
+
+testOpenSsl
+validateInput $1 $2
+createSslFolderIfNotExist
+exit 0
+
 generateRootCertConf > ./.ssl/root.cnf
 generateSingleCertConf > ./.ssl/server.cnf
